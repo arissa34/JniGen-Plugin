8cbf9bb4210fe943cef7a37fc23401dbc5d70cbf
GitHub Action <action@github.com>
2021-12-04 20:27:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/arissa34/jnigenplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/arissa34/jnigenplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.arissa34.jnigenplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arissa34.jnigenplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/arissa34/jnigenplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/arissa34/jnigenplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.arissa34.jnigenplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arissa34.jnigenplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
